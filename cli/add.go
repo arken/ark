99b8f18e9f19c65b854682862588d353d21af095
Danilo Hidalgo-Putnam <danilohidalgo@email.arizona.edu>
2020-09-15 22:41:25 -0700

    tentative multithreading of add

--- cli/add.go
+++ cli/add.go
@@ -2,13 +2,12 @@ package cli
 
 import (
 	"fmt"
+	"github.com/DataDrake/cli-ng/cmd"
+	"github.com/arkenproject/ait/utils"
+	"io/ioutil"
 	"os"
 	"path/filepath"
-	"strings"
-
-	"github.com/arkenproject/ait/utils"
-
-	"github.com/DataDrake/cli-ng/cmd"
+	"sync/atomic"
 )
 
 // Add imports a file or directory to AIT's local staging file.
@@ -25,6 +24,8 @@ type AddArgs struct {
 	Paths []string
 }
 
+var threads int32 = 0
+
 // AddRun Similar to "git add", this function adds files that match a given list of
 // file matching patterns (can include *, ? wildcards) to a file. Currently this
 // file is in .ait/added_files, and it contains paths relative to the program's
@@ -32,6 +33,7 @@ type AddArgs struct {
 // specific order of the filenames in the file is unpredictable, but users should
 // not be directly interacting with files in .ait anyway.
 func AddRun(_ *cmd.RootCMD, c *cmd.CMD) {
+	//runtime.GOMAXPROCS(2000) //macOS?
 	args := c.Args.(*AddArgs).Paths
 	contents := make(map[string]struct{}) //basically a set. empty struct has 0 width.
 	file := utils.BasicFileOpen(utils.AddedFilesPath, os.O_CREATE|os.O_RDONLY, 0644)
@@ -42,22 +44,20 @@ func AddRun(_ *cmd.RootCMD, c *cmd.CMD) {
 		userPath = filepath.Clean(userPath)
 		info, statErr := os.Stat(userPath)
 		_, alreadyContains := contents[userPath]
-		if !alreadyContains && !os.IsNotExist(statErr) && info != nil {
-			//if the path isn't already in the map and the file does exist
-			if info.IsDir() {
-				_ = filepath.Walk(userPath, func(diskPath string, info os.FileInfo, err error) error {
-					_, contains := contents[diskPath]
-					if !contains && !info.IsDir() && !strings.Contains(diskPath,
-						".ait" + string(filepath.Separator)) {
-						contents[diskPath] = struct{}{}
-						numAdded++
-					}
-					return nil
-				})
-			} else {
+		if !os.IsNotExist(statErr) && info != nil && !alreadyContains {
+			if !info.IsDir() {
 				contents[userPath] = struct{}{}
-				numAdded++
+			} else {
+				c := make(chan string)
+				atomic.AddInt32(&threads, 1)
+				go processDir(userPath, c)
+				for msg := range c {
+					contents[msg] = struct{}{}
+					numAdded++
+				}
 			}
+		} else if os.IsNotExist(statErr) {
+			fmt.Printf("Path \"%v\" found. Continuing...\n", userPath)
 		}
 	}
 	//completely truncate the file to avoid duplicated filenames
@@ -67,4 +67,21 @@ func AddRun(_ *cmd.RootCMD, c *cmd.CMD) {
 	err := utils.DumpMap(contents, file)
 	utils.CheckError(err)
 	fmt.Println(numAdded, "file(s) added")
+}
+
+func processDir(dir string, c chan string) {
+	files, err := ioutil.ReadDir(dir)
+	utils.CheckError(err)
+	for _, info := range files {
+		if info.IsDir() {
+			atomic.AddInt32(&threads, 1)
+			go processDir(filepath.Join(dir, info.Name()), c)
+		} else {
+			c <- info.Name()
+		}
+	}
+	atomic.AddInt32(&threads, -1)
+	if atomic.LoadInt32(&threads) <= 0 {
+		close(c)
+	}
 }
