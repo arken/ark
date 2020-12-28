7668d565bdfdada7b4633d4009a2be918ed23a82
Alec Scott <alec@bcs.sh>
2020-12-27 22:03:45 -0700

    Fix Release Script...Again

--- .github/workflows/release.sh
+++ .github/workflows/release.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 set -x
-assets =()
+assets=()
 for asset in ./ait-*; do
-    assets += ("-a" "$asset")
+    assets+=("-a" "$asset")
 done
 tag_name="${GITHUB_REF##*/}"
 hub release create "${assets[@]}" -m "$tag_name" "$tag_name"
