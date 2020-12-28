2bef97091585385874faa0a1aa9e004522740ab9
Alec Scott <alec@bcs.sh>
2020-12-27 22:00:12 -0700

    Switch Release to a Script as Well

--- .github/workflows/release.sh
+++ .github/workflows/release.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-platforms=("linux/amd64" "linux/arm" "linux/arm64" "darwin/amd64" "darwin/arm" "darwin/arm64")
-
-for platform in "${platforms[@]}"
-do
-    platform_split=(${platform//\// })
-    GOOS=${platform_split[0]}
-    GOARCH=${platform_split[1]}
-    go build -ldflags "-X github.com/arkenproject/ait/apis/github.clientID=$1 -X github.com/arkenproject/ait/cli.appVersion=$2" -o ait-$2-${GOOS}-${GOARCH} .
-
+set -x
+assets =()
+for asset in ./ait-*; do
+    assets += ("-a" "$asset")
 done
+tag_name="${GITHUB_REF##*/}"
+hub release create "${assets[@]}" -m "$tag_name" "$tag_name"
