87a0d5c0d30b3642651baee0b9dab25d5dee599f
Alec Scott <alec@bcs.sh>
2020-12-27 21:32:17 -0700

    Fix spacing in automation script.

--- .github/workflows/release.sh
+++ .github/workflows/release.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-platforms=("linux/amd64" "linux/arm" "linux/arm64" "darwin/amd64" "darwin/arm" "darwin/arm64")
+platforms = ("linux/amd64" "linux/arm" "linux/arm64" "darwin/amd64" "darwin/arm" "darwin/arm64")
 
 for platform in "${platforms[@]}"
 do
-    platform_split=(${platform//\// })
-    GOOS=${platform_split[0]}
-    GOARCH=${platform_split[1]}
+    platform_split = (${platform//\// })
+    GOOS = ${platform_split[0]}
+    GOARCH = ${platform_split[1]}
     go build -ldflags "-X github.com/arkenproject/ait/apis/github.clientID=$1 -X github.com/arkenproject/ait/cli.appVersion=$2" -o ait-v$2-${GOOS}-${GOARCH} .
 
 done
